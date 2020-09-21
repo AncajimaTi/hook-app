a2c8a51ae765635568dca7461ccdc9911ffa344f
AncajimaTi <punisherancajima@gmail.com>
2020-09-20 21:06:43 -0500

    subit

---
+++ docs/precache-manifest.17523395fb4cd5d5712ef49f7b257fbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8206882fb48350dbb36b945eb539c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d073dad38eeb2a921c2",
+    "url": "/static/css/main.4233041d.chunk.css"
+  },
+  {
+    "revision": "9609f607b7f67dafefa3",
+    "url": "/static/js/2.86c83d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86c83d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d073dad38eeb2a921c2",
+    "url": "/static/js/main.d21f668c.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
